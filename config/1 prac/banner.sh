b8d63807f28d1dc7089cc5e19e4d1c6062b98969
ONDREY <moiseev.andrey2005@gmail.com>
2024-09-16 01:00:29 +0300

    Update banner.sh

--- config/1 prac/banner.sh
+++ config/1 prac/banner.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
-# Функция для создания баннера
-banner() {
-  local text="$1"
-  local text_length=${#text}
-  local banner_width=$((text_length + 4))
-
-  # Выводим верхнюю границу
-  echo "+$(printf '%0.s-' $(seq 1 $banner_width))+"
-
-  # Выводим строку с текстом
-  echo "| $text |"
-
-  # Выводим нижнюю границу
-  echo "+$(printf '%0.s-' $(seq 1 $banner_width))+"
-}
-
-# Пример использования
-banner "Hello from RTU MIREA!"
+str1=$*
+len1=${#str1}
+echo -n "+-"
+for((a=1;a<len1;a++))
+do
+echo -n "-"
+done
+echo "-+"
+echo "| $str1 |"
+echo -n "+-"
+for((a=1;a<len1;a++))
+do
+echo -n "-"
+done
+echo "-+"

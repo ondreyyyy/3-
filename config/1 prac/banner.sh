5c40f9a09d9b0fb5de7a51184b96a2b38adfc5f5
ONDREY <moiseev.andrey2005@gmail.com>
2024-09-16 01:06:07 +0300

    Update banner.sh

--- config/1 prac/banner.sh
+++ config/1 prac/banner.sh
@@ -2,14 +2,14 @@
 str1=$*
 len1=${#str1}
 echo -n "+-"
-for((a=1;a<len1;a++))
+for ((a=1;a<=len1;a++))
 do
 echo -n "-"
 done
 echo "-+"
 echo "| $str1 |"
 echo -n "+-"
-for((a=1;a<len1;a++))
+for((a=1;a<=len1;a++))
 do
 echo -n "-"
 done

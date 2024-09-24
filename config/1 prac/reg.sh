fdb2d1231ced1a04f49d277233888cb56b1a60d2
ONDREY <moiseev.andrey2005@gmail.com>
2024-09-24 21:37:50 +0300

    Create reg.sh

---
+++ config/1 prac/reg.sh
@@ -0,0 +1,22 @@
+if [ -z "$1" ]; then
+    echo "Использование: $0 <путь к файлу>"
+    exit 1
+fi
+
+FILE="$1"
+
+if [ ! -f "$FILE" ]; then
+    echo "Ошибка: файл '$FILE' не найден."
+    exit 1
+fi
+
+chmod +x "$FILE"
+
+sudo cp "$FILE" /usr/local/bin/
+
+if [ $? -eq 0 ]; then
+    echo "Файл '$FILE' успешно зарегистрирован и скопирован в /usr/local/bin."
+else
+    echo "Ошибка при копировании файла."
+    exit 1
+fi

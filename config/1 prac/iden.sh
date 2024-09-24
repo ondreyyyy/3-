b39cb2a22d231a40ff97e6c042e2bc05c5860298
ONDREY <moiseev.andrey2005@gmail.com>
2024-09-24 21:35:36 +0300

    Create iden.sh

---
+++ config/1 prac/iden.sh
@@ -0,0 +1,12 @@
+```
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Использование: $0 <имя файла>"
+    exit 1
+fi
+if [ ! -f "$1" ]; then
+    echo "Ошибка: файл $1 не существует"
+    exit 1
+fi
+grep -o -E '\b[_a-zA-Z][_a-zA-Z0-9]*\b' "$1" | sort -u
+```
